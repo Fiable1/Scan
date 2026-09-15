7acc50bd02c3dd283c9e94646024d5c744b33079
Fiabl1 <fiablemunezero@gmail.com>
2026-09-15 10:50:01 +0200

    Version2

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/', permanent=False), name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('scans.urls')),
 ]
